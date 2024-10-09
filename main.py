1635f2b270b83891da177f2950c5b02425902285
BatOnDrugs <bartlomiej.gozdek@gmail.com>
2024-10-09 05:00:27 +0200

    shapes and stuff wip

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:    
         pygame.Surface.fill(screen,(0,0,0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
+        dt = clock.get_time() / 1000
+        
+       
+        
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
