1cf1e3c5773010ad350908506173e6e0b108ce6f
BatOnDrugs <bartlomiej.gozdek@gmail.com>
2024-10-10 04:59:06 +0200

    added asteroids

--- main.py
+++ main.py
@@ -4,16 +4,28 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
     while True:    
         pygame.Surface.fill(screen,(0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
         
         pygame.display.flip()
         
--- player.py
+++ player.py
@@ -24,8 +24,18 @@ class Player(CircleShape):
 
         if keys[pygame.K_a]:
             self.rotate(dt)
-            # ?
+            
         if keys[pygame.K_d]:
             self.rotate(dt*(-1))
-            # ?
+        
+        if keys[pygame.K_w]:
+            self.move(dt)
+        
+        if keys[pygame.K_s]:
+            self.move(dt*(-1))
+            
         pass
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
